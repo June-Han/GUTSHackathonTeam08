8344eb599290bb5b7152eb986cb8c4cbb592ff20
June-Han <julialiu08@gmail.com>
2021-02-06 17:49:05 +0800

    name activity

---
+++ Melody/app/src/main/java/com/team08/myapplication/name.kt
@@ -0,0 +1,11 @@
+package com.team08.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class name : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_name)
+    }
+}
