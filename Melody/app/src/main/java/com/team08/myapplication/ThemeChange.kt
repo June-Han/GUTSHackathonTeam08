e2179dd33731add8196110ebff2271483be16407
June-Han <julialiu08@gmail.com>
2021-02-06 21:10:38 +0800

    progress

--- Melody/app/src/main/java/com/team08/myapplication/ThemeChange.kt
+++ Melody/app/src/main/java/com/team08/myapplication/ThemeChange.kt
@@ -3,9 +3,9 @@ package com.team08.myapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class Name : AppCompatActivity() {
+class ThemeChange : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_name)
+        setContentView(R.layout.activity_theme_change)
     }
 }
