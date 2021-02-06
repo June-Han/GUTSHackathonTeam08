a813588ed14d33fab11e6eeffcc12c2be989f296
June-Han <julialiu08@gmail.com>
2021-02-06 19:35:19 +0800

    namecard

--- Melody/app/src/main/java/com/team08/myapplication/Name.kt
+++ Melody/app/src/main/java/com/team08/myapplication/Name.kt
@@ -1,4 +1,11 @@
 package com.team08.myapplication
 
-class Name {
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Name : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chat)
+    }
 }
