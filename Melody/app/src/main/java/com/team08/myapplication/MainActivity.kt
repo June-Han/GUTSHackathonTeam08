f0a8d2180b73b95331f291116bd61fc567af3f6d
gohruhui <gohruhui@hotmail.com>
2021-02-07 13:57:50 +0800

    Music functionality and bottom nav

--- Melody/app/src/main/java/com/team08/myapplication/MainActivity.kt
+++ Melody/app/src/main/java/com/team08/myapplication/MainActivity.kt
@@ -1,11 +1,40 @@
 package com.team08.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        PlayDefaultSound()
     }
+
+    fun chatBttnFn(view: View) {
+        val intent1 = Intent(this, Chat::class.java)
+        startActivity(intent1)
+    }
+
+    fun quoteBttnFn(view: View) {
+//        val intent2 = Intent(this, BackgroundSoundService::class.java)
+//        startActivity(intent2)
+    }
+
+    fun musicBttnFn(view: View) {
+        val intent3 = Intent(this, Music::class.java)
+        startActivity(intent3)
+    }
+
+    fun helplineBttnFn(view: View) {
+//        val intent4 = Intent(this, helpline::class.java)
+//        startActivity(intent4)
+    }
+
+    fun PlayDefaultSound() {
+        val intent = Intent(this, BackgroundSoundService::class.java)
+        startService(intent)
+    }
+
 }
