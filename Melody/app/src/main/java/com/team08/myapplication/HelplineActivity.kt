8481aaaa7b7c3897abfc0118e17babe8db864024
Chen Nili <chen_ni_li@hotmail.com>
2021-02-07 19:48:11 +0800

    Update Music and Helpline
    update the menu

--- Melody/app/src/main/java/com/team08/myapplication/HelplineActivity.kt
+++ Melody/app/src/main/java/com/team08/myapplication/HelplineActivity.kt
@@ -1,7 +1,9 @@
 package com.team08.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class HelplineActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -9,5 +11,24 @@ class HelplineActivity : AppCompatActivity() {
         setContentView(R.layout.activity_helpline)
 
         supportActionBar?.hide()
+    }
+    fun chatBttnFn(view: View) {
+        val intent1 = Intent(this, ChatActivity::class.java)
+        startActivity(intent1)
+    }
+
+    fun quoteBttnFn(view: View) {
+        val intent2 = Intent(this, Quotes::class.java)
+        startActivity(intent2)
+    }
+
+    fun musicBttnFn(view: View) {
+        val intent3 = Intent(this, Music::class.java)
+        startActivity(intent3)
+    }
+
+    fun helplineBttnFn(view: View) {
+        val intent4 = Intent(this, HelplineActivity::class.java)
+        startActivity(intent4)
     }
 }
