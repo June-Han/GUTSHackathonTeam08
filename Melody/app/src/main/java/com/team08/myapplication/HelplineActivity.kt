4a3d4bc5d853285d735b1eda0053abd9d7196cc0
June-Han <julialiu08@gmail.com>
2021-02-07 18:52:14 +0800

    add save state to helpline

--- Melody/app/src/main/java/com/team08/myapplication/HelplineActivity.kt
+++ Melody/app/src/main/java/com/team08/myapplication/HelplineActivity.kt
@@ -6,7 +6,13 @@ import android.os.Bundle
 class HelplineActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Utils.onActivityCreateSetTheme(this)
         setContentView(R.layout.activity_helpline)
+
+        val themeNo = savedInstanceState?.getInt("ThemeNo", 0)
+        if (themeNo != null) {
+            Utils.changeToTheme(this, themeNo)
+        }
 
         supportActionBar?.hide()
     }
