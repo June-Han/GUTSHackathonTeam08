d1a9e0c684008681f2a206b7ae04245a14906e0d
June-Han <julialiu08@gmail.com>
2021-02-07 11:11:07 +0800

    themechange

--- Melody/app/src/main/java/com/team08/myapplication/ThemeChange.kt
+++ Melody/app/src/main/java/com/team08/myapplication/ThemeChange.kt
@@ -2,10 +2,32 @@ package com.team08.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.team08.myapplication.Utils
 
 class ThemeChange : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Utils.onActivityCreateSetTheme(this);
         setContentView(R.layout.activity_theme_change)
+
+        //set Buttons to listeners
+        val seaButton = findViewById<Button>(R.id.ThemeBlue)
+        val sakuraButton = findViewById<Button>(R.id.ThemePink)
+        val forestButton = findViewById<Button>(R.id.ThemeGreen)
+        val aquaButton = findViewById<Button>(R.id.ThemeTurquoise)
+
+        seaButton.setOnClickListener{
+            Utils.changeToTheme(this, Utils.themeSea);
+        }
+        sakuraButton.setOnClickListener{
+            Utils.changeToTheme(this, Utils.themeSakura);
+        }
+        forestButton.setOnClickListener{
+            Utils.changeToTheme(this, Utils.themeForest);
+        }
+        aquaButton.setOnClickListener{
+            Utils.changeToTheme(this, Utils.themeAqua);
+        }
     }
 }
